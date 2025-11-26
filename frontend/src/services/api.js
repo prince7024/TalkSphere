5b454a00ed44daada9b55f5467682797c454233e
Prince Kumar Choudhary <princekumarchoudhary22@gmail.com>
2025-11-27 04:04:13 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL:  `${import.meta.env.VITE_BACKEND_URL}/api`,
   withCredentials: true,
-  timeout: 20000,
+  timeout: 60000,
 });
