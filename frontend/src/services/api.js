52542fe1fed44340c3f80bee415fba481420f8c5
Prince Kumar Choudhary <princekumarchoudhary22@gmail.com>
2026-01-06 16:29:29 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
-  timeout: 60000,
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
+  timeout: 20000,
 });
-
-// Attach token automatically to every request
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token"); // or authToken
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
